f403f2348b430911d32ed4cc3fba3669d1790130
Evan Izaksonas-Smith <izak0002@umn.edu>
2013-01-21 14:34:11 -0600

    Simplified functions and removed redunancies. * let-recur.el: (let-recur--tc) is no longer used.  (let-recur--transform) now   explicitly throws.  (pcase-recur) added.

--- let-recur.el
+++ let-recur.el
@@ -1,10 +1,10 @@
-;;; let-recur.el --- Simplified implementation of recur
+;;; let-recur.el --- Simplified implementation of recur -*- lexical-binding:t -*-
 ;; Copyright (C) 2012  Evan Izaksonas-Smith
 
 ;; Author: Evan Izaksonas-Smith <izak0002 at umn dot edu>
 ;; Maintainer: Evan Izaksonas-Smith
 ;; Created: 1st Januay 2012
-;; Version: 0.0.2
+;; Version: 0.0.5
 ;; Keywords: lisp
 ;; Description: A simplified let-recur mechanic.
 
@@ -32,42 +32,65 @@
 (eval-when-compile
   (require 'cl))
 
-(defun let-recur--tc
-  (&rest args) "Utility function, not intended for external use."
-  (throw :recur args))
-
 (defun let-recur--transform (form)
   "Blindly looks for :recur in the function slot of an sexp.
 Will blithely transform it in places where it will break your
 code.  So don't put the keyword :recur in your code unless you
 are certain it is a tail call."
-  (if (consp form)
-      (if (eq :recur (car form))
-        `(let-recur--tc ,@(cdr form))
-        (cons (car form) (mapcar 'let-recur--transform (cdr form))))
-      form))             
+  (pcase form
+    (`(:recur . ,args)    
+     `(throw :recur (list ,@args)))
+    (`(,func . ,args)
+     `(,func ,@(mapcar 'let-recur--transform args)))
+    (_ form)))
 
 (defmacro let-recur (bindings &rest body)
-  "Like let but acts as a lambda named :recur, however :recur will
-only work correctly in the atil-call position, so this is only
-suitable for recursive functions that are guaranteed to be
+  "Like `let' but acts as a lambda named :recur, however :recur
+will only work correctly in the tail-call position, so this is
+only suitable for recursive functions that are guaranteed to be
 strictly tail recursive.  Using the keyword :recur anywhere else
 in the body will yield undefined behavior."
   (declare (indent 2))
   (setcar (last body)
           (let-recur--transform (car (last body))))
   (let ((arglist (mapcar #'first bindings))
-        (args (gensym))
-        (real-call (gensym)))
+        (args (gensym "args")))
     `(let ((,args (list ,@(mapcar #'second bindings))))
-       (let ((,real-call ,args))
-         (catch :return
-           (while t
-             (setq ,real-call
-                   (catch :recur
-                     (throw :return
-                            (apply (lambda ,arglist ,@body)
-                                   ,real-call))))))))))
+       (catch :return
+         (while t
+           (setq ,args
+                 (catch :recur
+                   (throw :return
+                          (apply (lambda ,arglist ,@body)
+                                 ,args)))))))))
+
+(defmacro pcase-recur (exp &rest cases)
+  "Like `pcase' but acts as a lambda named :recur, however :recur
+will only work correctly in the tail-call position, so this is
+only suitable for recursive functions that are guaranteed to be
+strictly tail recursive.  Using the keyword :recur anywhere else
+in the body will yield undefined behavior."
+  (declare (indent 1))
+  (mapc (lambda (body)
+          (setcar (last body)
+                  (let-recur--transform (car (last body)))))
+        cases)
+  (let ((args (gensym "args")))
+    `(let ((,args ,exp))
+       (catch :return
+         (while t
+           (setq ,args
+                 (catch :recur
+                   (throw :return
+                          (pcase ,args ,@cases)))))))))
+
+;;; example:
+;; (defun preverse (list)
+;;   (pcase-recur `(,list)
+;;     (`(nil ,reverse) reverse)
+;;     (`((,head \, tail) ,reverse) (:recur tail `(,head ,@reverse)))
+;;     (`(,sequence) (:recur sequence nil))))
+
 
 (provide 'let-recur)
 ;;; let-recur.el ends here
